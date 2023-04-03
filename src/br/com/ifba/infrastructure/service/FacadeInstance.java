9bb30fd8bc3960fc1bc620b0af77e8f499f801de
VitorDanielA <vdanieldev1@gmail.com>
2023-04-03 13:26:55 -0300

    Alterando a view do sistema

--- src/br/com/ifba/infrastructure/service/FacadeInstance.java
+++ src/br/com/ifba/infrastructure/service/FacadeInstance.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package br.com.ifba.salmos.infrastructure.service;
+package br.com.ifba.infrastructure.service;
 
 /**
  *
